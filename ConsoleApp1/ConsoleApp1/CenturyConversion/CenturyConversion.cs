df25ff9dfc89ef8005c4e456f84be3b8c0ea616e
she0930she <she0930she@hotmail.com>
2024-02-21 16:28:02 -0800

    century conversion, assign 1

---
+++ ConsoleApp1/ConsoleApp1/CenturyConversion/CenturyConversion.cs
@@ -0,0 +1,20 @@
+namespace ConsoleApp1.CenturyConvertion
+{
+    public class CenturyConversion
+    {
+        private int Century { get; set; }
+        private int Years { get; set; }
+        private long Days { get; set; }
+        private long Hours { get; set; }
+        private long Minutes { get; set; }
+        private long Seconds { get; set; }
+        private long Milliseconds { get; set; }
+        private long Microseconds { get; set; }
+        private long Nanoseconds { get; set; }
+
+        public void CenturyConversionDetail(int century)
+        {
+            
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,11 +8,47 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
+            
+            // century conversion
+            
+            
+            // Number sizes and ranges
+            Console.WriteLine($"sbyte size:  {sizeof(sbyte)}, MinValue: {sbyte.MinValue}, MaxValue: {sbyte.MaxValue}");
+            Console.WriteLine($"byte size:  {sizeof(byte)}, MinValue: {byte.MinValue}, MaxValue: {byte.MaxValue}");
+            Console.WriteLine($"short size:  {sizeof(short)}, MinValue: {short.MinValue}, MaxValue: {short.MaxValue}");
+            Console.WriteLine($"ushort size:  {sizeof(ushort)}, MinValue: {ushort.MinValue}, MaxValue: {short.MaxValue}");
+            Console.WriteLine($"int size:  {sizeof(int)}, MinValue: {int.MinValue}, MaxValue: {int.MaxValue}");
+            Console.WriteLine($"uint size:  {sizeof(uint)}, MinValue: {uint.MinValue}, MaxValue: {uint.MaxValue}");
+            Console.WriteLine($"long size:  {sizeof(long)}, MinValue: {long.MinValue}, MaxValue: {long.MaxValue}");
+            Console.WriteLine($"ulong size:  {sizeof(ulong)}, MinValue: {ulong.MinValue}, MaxValue: {ulong.MaxValue}");
+            Console.WriteLine($"float size:  {sizeof(float)}, MinValue: {float.MinValue}, MaxValue: {float.MaxValue}");
+            Console.WriteLine($"double size:  {sizeof(double)}, MinValue: {double.MinValue}, MaxValue: {double.MaxValue}");
+            Console.WriteLine($"decimal size:  {sizeof(decimal)}, MinValue: {decimal.MinValue}, MaxValue: {decimal.MaxValue}");
+            byte a = 255;
+            //byte a = -1;
+            char b = 'g';
+            
+            
+            
+            // ReadLine and WriteLine
+            // string feeling; //declaring variable feeling           
+            //
+            // Console.WriteLine("Hey, how do you feel today?");            
+            //
+            // feeling = Console.ReadLine(); // takes the input from the user           
+            //
+            // Console.WriteLine("Hello there! I am feeling "+ feeling + "!"); // print the output 
+            
+            
+            
+
+
+            Console.WriteLine("********");
             ParamPassing demo = new ParamPassing();
             // out Param
             string str;
-            Console.WriteLine(demo.IsAuthentic("Anjila", "Antra123", out str));
-            Console.WriteLine(str);
+            // Console.WriteLine(demo.IsAuthentic("Anjila", "Antra123", out str));
+            // Console.WriteLine(str);
             // Console.WriteLine(demo.AddNumbers(20,30));
             // Console.WriteLine(demo.AddNumbers(20,30,30,40));
 
